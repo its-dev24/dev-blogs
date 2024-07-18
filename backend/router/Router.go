99ffef9a7f404d01d8327c2691bc1d431f91f180
Gautham <gauthamskaniyattil@gmail.com>
2024-07-18 21:24:38 +0530

    Fix : Router - DeleteABlog
    The Router path for delete a blog route was fixed

--- backend/router/Router.go
+++ backend/router/Router.go
@@ -11,6 +11,7 @@ func Router() *http.ServeMux {
 	mux.HandleFunc("GET /", controllers.HomeScreen)
 	mux.HandleFunc("GET /api/blogs", controllers.FetchAllBlogs)
 	mux.HandleFunc("DELETE /api/blogs", controllers.DeleteAllBlogs)
+	mux.HandleFunc("DELETE /api/blogs/{id}", controllers.DeleteABlog)
 	mux.HandleFunc("PUT /api/blogs/{id}", controllers.UpdateBlog)
 	return &mux
 }
